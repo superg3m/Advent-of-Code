c58c9ac35ba1da58420f0f4fdab1c15bd5cd1b03
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-10 14:30:04 -0400

    Update main.py

--- Advent-of-Code (2021)/Day2/Python/main.py
+++ Advent-of-Code (2021)/Day2/Python/main.py
@@ -1,9 +1,39 @@
 def part_one(lines: list[str]) -> int:
-    return 0
+    horizontal_count = 0
+    depth = 0
+    for line in lines:
+        direction_value = line.split(" ")
+        direction = direction_value[0]
+        value = int(direction_value[1])
+
+        if direction == "forward":
+            horizontal_count += value
+        elif direction == "down":
+            depth += value
+        elif direction == "up":
+            depth -= value
+
+    return horizontal_count * depth
 
 
 def part_two(lines: list[str]) -> int:
-    return 0
+    aim: int = 0
+    depth: int = 0
+    horizontal_count: int = 0
+    for line in lines:
+        direction_value = line.split(" ")
+        direction = direction_value[0]
+        x = int(direction_value[1])
+
+        if direction == "forward":
+            horizontal_count += x
+            depth += aim * x
+        elif direction == "down":
+            aim += x
+        elif direction == "up":
+            aim -= x
+
+    return horizontal_count * depth
 
 
 if __name__ == "__main__":
