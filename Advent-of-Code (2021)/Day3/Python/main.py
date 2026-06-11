a0ba368342016b1e56667fde3cfabdf3382be8ee
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-11 11:44:31 -0400

    day 3

--- Advent-of-Code (2021)/Day3/Python/main.py
+++ Advent-of-Code (2021)/Day3/Python/main.py
@@ -21,9 +21,61 @@ def part_one(lines: list[str]) -> int:
     epsilon_rate = bitmask & ~gamma_rate
     return gamma_rate * epsilon_rate
 
+def create_valid_list(lines: list[str], bit_position: int, bit: int) -> list[str]:
+    ret: list[str] = []
+
+    for line in lines:
+        if int(line[bit_position]) == bit:
+            ret.append(line)
+
+    return ret
 
 def part_two(lines: list[str]) -> int:
-    return 0
+    data = lines
+    for bit_position in range(len(data[0])):
+        if len(data) == 1:
+            break
+
+        one_bits_set: list[int] = [0] * len(data[0])
+        for line in data:
+            for i in range(len(line)):
+                bit = line[i]
+                one_bits_set[i] += int(bit)
+
+        for i, num in enumerate(one_bits_set):
+            one_bits_set[i] = num >= ((len(data) + 1) // 2)
+
+        data = create_valid_list(data, bit_position, one_bits_set[bit_position])
+
+    oxygen_rating = 0
+    for i in range(len(data[0])):
+        num = int(data[0][i])
+        if num:
+            oxygen_rating = set_bit(oxygen_rating, (len(data[0]) - 1) - i)
+
+    data = lines
+    for bit_position in range(len(data[0])):
+        if len(data) == 1:
+            break
+
+        one_bits_set: list[int] = [0] * len(data[0])
+        for line in data:
+            for i in range(len(line)):
+                bit = line[i]
+                one_bits_set[i] += int(bit)
+
+        for i, num in enumerate(one_bits_set):
+            one_bits_set[i] = num < ((len(data) + 1) // 2)
+
+        data = create_valid_list(data, bit_position, one_bits_set[bit_position])
+
+    co2_rating = 0
+    for i in range(len(data[0])):
+        num = int(data[0][i])
+        if num:
+            co2_rating = set_bit(co2_rating, (len(data[0]) - 1) - i)
+
+    return oxygen_rating * co2_rating
 
 
 if __name__ == "__main__":
