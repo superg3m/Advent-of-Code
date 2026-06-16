a513aafad1c6c4e38b86d8e96421036657a03d3d
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-16 19:25:43 -0400

    Day 11

--- Advent-of-Code (2021)/Day10/Python/main.py
+++ Advent-of-Code (2021)/Day10/Python/main.py
@@ -1,88 +1,131 @@
 from typing import Tuple
 
+SIZE = 10
+FLASH_COUNT = 0
+
+def perform_octopus_update(octopus_energy: list[int], flashed_this_step: list[bool], i: int, j: int):
+    global FLASH_COUNT
+
+    index = (i * SIZE) + j
+    if flashed_this_step[index]:
+        return
+
+    octopus_energy[index] += 1
+    if octopus_energy[index] == 10: #flash
+        FLASH_COUNT += 1
+        octopus_energy[index] = 0
+        flashed_this_step[index] = True
+
+        if i != 0 and j != 0: # diagonal_top_left
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j - 1)
+
+        if i != 0 and j != SIZE - 1: # diagonal_top_right
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j + 1)
+
+        if i != SIZE - 1 and j != 0:  # diagonal_bottom_left
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j - 1)
+
+        if i != SIZE - 1 and j != SIZE - 1:  # diagonal_bottom_right
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j + 1)
+
+        if j != 0:
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j - 1) # left
+
+        if i != 0:
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j) # up
+
+        if j != SIZE - 1:
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j + 1) # right
+
+        if i != SIZE - 1:
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j) # down
+
 def part_one(lines: list[str]) -> int:
-    closing_syntax_stack = []
+    octopus_energy: list[int] = [0] * (SIZE*SIZE)
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line):
+            octopus_energy[(i*SIZE) + j] = int(c)
 
-    open_to_closing = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        "<": ">"
-    }
+    for step in range(100):
+        flashed_this_step: list[bool] = [False] * (SIZE*SIZE)
+        for index, _ in enumerate(octopus_energy):
+            i = index // SIZE
+            j = index % SIZE
 
-    closing_to_open = {
-        ")": "(",
-        "]": "[",
-        "}": "{",
-        ">": "<"
-    }
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j)
 
-    close_syntax_to_points = {
-        ")": 3,
-        "]": 57,
-        "}": 1197,
-        ">": 25137
-    }
+    for i in range(SIZE*SIZE):
+        if i != 0 and i % SIZE == 0:
+            print()
+            print(octopus_energy[i], end="")
+        else:
+            print(octopus_energy[i], end="")
 
-    invalid_points = []
-    for line in lines:
-        for c in line:
-            if c in ["(", "[", "{", "<"]:
-                closing_syntax_stack.append(open_to_closing[c])
+    print()
 
-            if c in [")", "]", "}", ">"]:
-                expected = closing_syntax_stack.pop()
-                if c != expected:
-                    invalid_points.append(close_syntax_to_points[c])
-                    break
+    for i in range(SIZE):
+        print(octopus_energy[i * SIZE:(i + 1) * SIZE])
 
-    return sum(invalid_points)
+    return FLASH_COUNT
+
+
+def perform_octopus_update(octopus_energy: list[int], flashed_this_step: list[bool], i: int, j: int):
+    global FLASH_COUNT
+
+    index = (i * SIZE) + j
+    if flashed_this_step[index]:
+        return
+
+    octopus_energy[index] += 1
+    if octopus_energy[index] == 10: #flash
+        FLASH_COUNT += 1
+        octopus_energy[index] = 0
+        flashed_this_step[index] = True
+
+        if i != 0 and j != 0: # diagonal_top_left
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j - 1)
+
+        if i != 0 and j != SIZE - 1: # diagonal_top_right
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j + 1)
+
+        if i != SIZE - 1 and j != 0:  # diagonal_bottom_left
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j - 1)
+
+        if i != SIZE - 1 and j != SIZE - 1:  # diagonal_bottom_right
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j + 1)
+
+        if j != 0:
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j - 1) # left
+
+        if i != 0:
+            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j) # up
+
+        if j != SIZE - 1:
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j + 1) # right
+
+        if i != SIZE - 1:
+            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j) # down
 
 def part_two(lines: list[str]) -> int:
-    closing_syntax_stack = []
+    octopus_energy: list[int] = [0] * (SIZE*SIZE)
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line):
+            octopus_energy[(i*SIZE) + j] = int(c)
 
-    open_to_closing = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        "<": ">"
-    }
+    step = 0
+    while True:
+        step += 1
 
-    close_syntax_to_points = {
-        ")": 1,
-        "]": 2,
-        "}": 3,
-        ">": 4
-    }
+        flashed_this_step: list[bool] = [False] * (SIZE*SIZE)
+        for index, _ in enumerate(octopus_energy):
+            i = index // SIZE
+            j = index % SIZE
 
-    points = []
-    for line in lines:
-        closing_syntax_stack = []
-        invalid = False
-        for c in line:
-            if c in ["(", "[", "{", "<"]:
-                closing_syntax_stack.append(open_to_closing[c])
+            perform_octopus_update(octopus_energy, flashed_this_step, i, j)
 
-            if c in [")", "]", "}", ">"]:
-                expected = closing_syntax_stack.pop()
-                if c != expected:
-                    invalid = True
-                    break
+        if sum(octopus_energy) == 0:
+            return step
 
-            if invalid:
-                break
-
-        if not invalid and len(closing_syntax_stack) != 0:
-            total_points = 0
-            while len(closing_syntax_stack) != 0:
-                total_points *= 5
-                total_points += close_syntax_to_points[closing_syntax_stack.pop()]
-
-            points.append(total_points)
-
-    points.sort()
-
-    return points[(len(points) - 1) // 2]
 
 if __name__ == "__main__":
     f = open("../Day10.txt")
