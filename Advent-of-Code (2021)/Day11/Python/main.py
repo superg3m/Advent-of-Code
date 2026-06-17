66c12a174a295baee1cbbb849839439844663db7
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-16 22:57:18 -0400

    day 12

--- Advent-of-Code (2021)/Day11/Python/main.py
+++ Advent-of-Code (2021)/Day11/Python/main.py
@@ -56,44 +56,6 @@ def part_one(lines: list[str]) -> int:
 
     return FLASH_COUNT
 
-
-def perform_octopus_update(octopus_energy: list[int], flashed_this_step: list[bool], i: int, j: int):
-    global FLASH_COUNT
-
-    index = (i * SIZE) + j
-    if flashed_this_step[index]:
-        return
-
-    octopus_energy[index] += 1
-    if octopus_energy[index] == 10: #flash
-        FLASH_COUNT += 1
-        octopus_energy[index] = 0
-        flashed_this_step[index] = True
-
-        if i != 0 and j != 0: # diagonal_top_left
-            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j - 1)
-
-        if i != 0 and j != SIZE - 1: # diagonal_top_right
-            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j + 1)
-
-        if i != SIZE - 1 and j != 0:  # diagonal_bottom_left
-            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j - 1)
-
-        if i != SIZE - 1 and j != SIZE - 1:  # diagonal_bottom_right
-            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j + 1)
-
-        if j != 0:
-            perform_octopus_update(octopus_energy, flashed_this_step, i, j - 1) # left
-
-        if i != 0:
-            perform_octopus_update(octopus_energy, flashed_this_step, i - 1, j) # up
-
-        if j != SIZE - 1:
-            perform_octopus_update(octopus_energy, flashed_this_step, i, j + 1) # right
-
-        if i != SIZE - 1:
-            perform_octopus_update(octopus_energy, flashed_this_step, i + 1, j) # down
-
 def part_two(lines: list[str]) -> int:
     octopus_energy: list[int] = [0] * (SIZE*SIZE)
     for i, line in enumerate(lines):
---
+++ Advent-of-Code (2021)/Day12/Python/main.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+def part_one(lines: list[str]) -> int:
+    return 0
+
+def part_two(lines: list[str]) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    f = open("../Day12.txt")
+    lines = f.read().splitlines()
+    print(f"part1: {part_one(lines)}")
+    print(f"part2: {part_two(lines)}")
+    f.close()
