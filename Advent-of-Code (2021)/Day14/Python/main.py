5a6a36cc470a3f9047ba0246c3bc4c7792380edc
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-17 23:56:39 -0400

    Update main.py

--- Advent-of-Code (2021)/Day14/Python/main.py
+++ Advent-of-Code (2021)/Day14/Python/main.py
@@ -10,8 +10,8 @@ def perform_polymer_insertion_step(polymer_template: str, pair_insertion_rules: 
             to_insert.append((i, pair_insertion_rules[compound]))
 
     ret = polymer_template[:]
-    for pair in list(reversed(to_insert)):
-        i, insert = pair
+    for index in range(len(to_insert)):
+        i, insert = to_insert[(len(to_insert) - 1) - index]
         ret = ret[:i + 1] + insert + ret[i + 1:]
 
     return ret
@@ -53,6 +53,20 @@ def part_one(lines: list[str]) -> int:
 
     return most_common - least_common
 
+def perform_polymer_insertion_step2(polymer_template: str, pair_insertion_rules: Dict[str, str]) -> str:
+    to_insert = []
+    for i in range(len(polymer_template) - 1):
+        compound = polymer_template[i:i+2]
+        if compound in pair_insertion_rules:
+            to_insert.append((i, pair_insertion_rules[compound]))
+
+    ret = polymer_template[:]
+    for index in range(len(to_insert)):
+        i, insert = to_insert[(len(to_insert) - 1) - index]
+        ret = ret[:i + 1] + insert + ret[i + 1:]
+
+    return ret
+
 def part_two(lines: list[str]) -> int:
     polymer_template = lines[0]
 
@@ -63,8 +77,8 @@ def part_two(lines: list[str]) -> int:
         pair_insertion_rules[left] = right
 
     # print(f"Template: {polymer_template}")
-    for step in range(40):
-        polymer_template = perform_polymer_insertion_step(polymer_template, pair_insertion_rules)
+    for step in range(14):
+        polymer_template = perform_polymer_insertion_step2(polymer_template, pair_insertion_rules)
         # print(f"After step {step + 1}: {polymer_template}")
 
     most_common, least_common = most_common_element_least_common_element(polymer_template)
