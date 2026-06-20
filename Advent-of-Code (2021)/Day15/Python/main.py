c4dc8eb27ec3d7977dcd1404bd6f01c3706ffc28
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-20 12:58:54 -0400

    ouch

--- Advent-of-Code (2021)/Day15/Python/main.py
+++ Advent-of-Code (2021)/Day15/Python/main.py
@@ -1,27 +1,40 @@
-from typing import get_origin
+import heapq
+from typing import Tuple
 
+from idna import valid_label_length
 
-def dp(grid: list[list[int]], memo: list[list[int]], value: int, i: int, j: int):
-    value += grid[i][j]
-    if memo[i][j] == 0:
-        memo[i][j] = value
-    elif value < memo[i][j]:
-        memo[i][j] = value
-    else:
-        return
+def create_index_from_i_and_j(i, j, width) -> int:
+    return (i * width) + j
 
-    if j < len(grid[0]) - 1:
-        dp(grid, memo, value, i, j + 1)  # right
+def compute_adjacency_list_from_grid(adj: list[list[Tuple[int, float]]], grid: list[list[int]]):
+    width = len(grid[0])
 
-    if i < len(grid) - 1:
-        dp(grid, memo, value, i + 1, j)  # left
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            index = (i * len(grid[i])) + j
+
+            if j > 0:
+                adj[index].append((create_index_from_i_and_j(i, j - 1, width), float(grid[i][j - 1]))) # left
+
+            if i > 0:
+                adj[index].append((create_index_from_i_and_j(i - 1, j, width), float(grid[i - 1][j]))) # up
+
+            if j < len(grid[i]) - 1:
+                adj[index].append((create_index_from_i_and_j(i, j + 1, width), float(grid[i][j + 1])))  # right
+
+            if i < len(grid[i]) - 1:
+                adj[index].append((create_index_from_i_and_j(i + 1, j, width), float(grid[i + 1][j])))  # down
+
+def dijkstra(adj: list[list[Tuple[int, float]]], width: int, height: int):
+    # wow i i'm just not smart at times...
+
+    pass
 
 def part_one(lines: list[str]) -> int:
     height = len(lines)
     width = len(lines[0])
 
     grid: list[list[int]] = []
-    memo: list[list[int]] = [[0 for _ in range(width)] for _ in range(height)]
     for line in lines:
         row: list[int] = []
         for element in line:
@@ -29,11 +42,10 @@ def part_one(lines: list[str]) -> int:
 
         grid.append(row)
 
-
-    dp(grid, memo, 0, 0, 0)
-
-    for row in memo:
-        print(row)
+    adj = [[] for _ in range(len(grid) * len(grid[0]))]
+    compute_adjacency_list_from_grid(adj, grid)
+    min_distances = dijkstra(adj, width, height)
+    print(min_distances)
 
     return 0
 
