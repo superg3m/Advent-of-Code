0d97886da9e2d28cf07e648214fb0f885fe62967
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-06-22 18:16:20 -0400

    Update main.py

--- Advent-of-Code (2021)/Day16/Python/main.py
+++ Advent-of-Code (2021)/Day16/Python/main.py
@@ -1,6 +1,5 @@
 from typing import Dict, Tuple
 
-
 def hex_to_binary_string(hex: str) -> str:
     hex_to_binary: Dict[str, str] = {
         "0": "0000",
@@ -37,20 +36,48 @@ def binary_str_to_decimal(binary_str: str) -> int:
 
     return ret
 
+class Packet:
+    version: int = -1
+    type_id: int = -1
+
+    string_bits: str = ""
+
+class LiteralValuePacket(Packet):
+    value: int = 0
+
+    def __init__(self, string_bits: str, version: int, type_id: int, value: int):
+        self.string_bits = string_bits
+        self.version = version
+        self.type_id = type_id
+        self.value = value
+
+class OperatorPacket(Packet):
+    length_type_id: int = 0
+    sub_packets: list[Packet] = []
+
+    def __init__(self, string_bits: str, version: int, type_id: int, length_type_id: int, sub_packets: list[Packet]):
+        self.string_bits = string_bits
+        self.version = version
+        self.type_id = type_id
+        self.length_type_id = length_type_id
+        self.sub_packets = sub_packets
+
 def binary_to_version_type_id(binary_str: str) -> Tuple[int, int, str]:
     return binary_str_to_decimal(binary_str[0:3]), binary_str_to_decimal(binary_str[3:6]), binary_str[6:len(binary_str)]
 
-def consume_packet_binary_literal(packet: str) -> Tuple[str, str]:
+def get_packet_binary_literal(packet_binary: str) -> str:
+    packet_without_header = consume_packet_header(packet_binary)
+
     i: int = 0
     while True:
-        first = int(packet[i])
+        first = int(packet_without_header[i])
         i += 5
         if first == 0:
             break
 
-    return packet[i:len(packet)], packet[0:i]
+    return packet_without_header[0:i]
 
-def decimal_from_binary_literal_bits(binary_literal_bits: str) -> int:
+def binary_literal_bits_to_decimal(binary_literal_bits: str) -> int:
     groups = []
 
     i: int = 0
@@ -66,20 +93,19 @@ def decimal_from_binary_literal_bits(binary_literal_bits: str) -> int:
     return binary_str_to_decimal("".join(groups))
 
 # get the next index
-def consume_packet_header(packet: str) -> Tuple[str, int, int]:
-    return packet[6:len(packet)], binary_str_to_decimal(packet[0:3]), binary_str_to_decimal(packet[3:6])
+def get_packet_header(packet_binary: str) -> Tuple[int, int]:
+    return binary_str_to_decimal(packet_binary[0:3]), binary_str_to_decimal(packet_binary[3:6])
 
-def consume_packet_length_type_id(packet: str) -> Tuple[str, int]:
-    return packet[1:len(packet)], int(packet[0])
+def get_packet_length_of_sub_packets_in_bits(packet_binary: str) -> int:
+    return binary_str_to_decimal(packet_binary[8:23])
 
-def consume_packet_length_of_sub_packets_in_bits(packet: str) -> Tuple[str, int]:
-    return packet[15:len(packet)], binary_str_to_decimal(packet[0:15])
+def get_packet_count_of_sub_packets(packet_binary: str) -> int:
+    return binary_str_to_decimal(packet_binary[8:19])
 
-def consume_packet_count_of_sub_packets(packet: str) -> Tuple[str, int]:
-    return packet[11:len(packet)], binary_str_to_decimal(packet[0:11])
+def consume_packet_header(packet_binary: str) -> str:
+    return packet_binary[6:len(packet_binary)]
 
-HEADER_LENGTH = 6
-
+"""
 def consume_packet(VERSION_ARRAY: list[int], packet: str) -> Tuple[str, str]:
     packet, version, type_id = consume_packet_header(packet)
     VERSION_ARRAY.append(version)
@@ -103,14 +129,46 @@ def consume_packet(VERSION_ARRAY: list[int], packet: str) -> Tuple[str, str]:
                 total_sub_packets_count += 1
 
     return packet, ""
+"""
+
+def create_packet(packet_binary: str) -> Packet:
+    version, type_id = get_packet_header(packet_binary)
+    if type_id == 4: #literal
+        binary_literal_bits = get_packet_binary_literal(packet_binary)
+        bit_length = 6 + len(binary_literal_bits)
+        value = binary_literal_bits_to_decimal(binary_literal_bits)
+
+        return LiteralValuePacket(packet_binary[0:bit_length], version, type_id, value)
+
+    sub_packets: list[Packet] = []
+    length_type_id = int(packet_binary[7])
+    if length_type_id == 0:
+        sub_packets_bit_length = get_packet_length_of_sub_packets_in_bits(packet_binary)
+        total_sub_packets_bits = 0
+
+        start_index = 8
+        while total_sub_packets_bits < sub_packets_bit_length:
+            sub_packet = create_packet(packet_binary[8 + total_sub_packets_bits:len(packet_binary)])
+            total_sub_packets_bits += len(sub_packet.string_bits)
+            sub_packets.append(sub_packet)
+    else:
+        sub_packet_count = get_packet_count_of_sub_packets(packet_binary)
+        total_sub_packets_count = 0
+        total_sub_packets_bits = 0
+        while total_sub_packets_count < sub_packet_count:
+            sub_packet = create_packet(packet_binary[8 + total_sub_packets_bits:len(packet_binary)])
+            sub_packets.append(sub_packet)
+            total_sub_packets_bits += len(sub_packet.string_bits)
+            total_sub_packets_count += 1
+
+    return OperatorPacket(packet_binary, version, type_id, length_type_id, sub_packets)
 
 def part_one(lines: list[str]) -> int:
     hex = lines[0]
-    packet = hex_to_binary_string(hex)
-    VERSION_ARRAY = []
-    consume_packet(VERSION_ARRAY, packet)
+    packet_binary = hex_to_binary_string(hex)
+    packet = create_packet(packet_binary)
 
-    return sum(VERSION_ARRAY)
+    return 0
 
 def part_two(lines: list[str]) -> int:
     return 0
